03c0c1a3c68aa898dd37dc3e04fce3555fed7750
Shreyab17 <21r11a66f1@gcet.edu.in>
2024-11-03 00:04:17 +0530

    updated

--- index.js
+++
@@ -1,9 +0,0 @@
-function toggleServiceType() {
-    const serviceSelect = document.getElementById('services');
-    const serviceTypeSelect = document.getElementById('service-type');
-    if (serviceSelect.value === 'home') {
-        serviceTypeSelect.disabled = true;
-    } else {
-        serviceTypeSelect.disabled = false;
-    }
-}
